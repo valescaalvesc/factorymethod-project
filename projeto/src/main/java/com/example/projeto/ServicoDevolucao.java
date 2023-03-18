6074f1e4e5b79962f7434554d55e488f4e96ab47
valescaalvesc <valescaalvess@gmail.com>
2023-03-18 07:16:38 -0700

    devolucao

---
+++ projeto/src/main/java/com/example/projeto/ServicoDevolucao.java
@@ -0,0 +1,12 @@
+package com.example.projeto;
+
+public class ServicoDevolucao {
+
+    public String salvar() {
+        return "Devolução efetivada";
+    }
+
+    public String excluir() {
+        return "Devolução cancelada";
+    }
+}
